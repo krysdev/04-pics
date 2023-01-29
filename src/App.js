cd03dd32b0999f587b1b25e50eb712d45ef04d1e
KrysDev <webdevkrys@gmail.com>
2023-01-29 15:31:34 +0000

    x

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 // import ImageList from "./components/ImageList";
+import searchImages from "./api";
 import SearchBar from "./components/SearchBar";
 
-const handleSubmit = (searchTerm) => { 
-  console.log('Search for', searchTerm)
+const handleSubmit = (searchTerm) => { // pass this function to SearchBar.js through props
+  searchImages(searchTerm)
+  
+  // console.log('Search for', searchTerm)
 };
 
 function App() {
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,23 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
 function SearchBar({ whenPressEnter }) {
+  const [term, setTerm] = useState('');
+  
   const handleSubmitForm = (e) => {
     e.preventDefault();
-    whenPressEnter('cars');
-
+    whenPressEnter(term);
     // console.log('ENTER pressed')
+  };
+
+  const handleChange = (event) => { 
+    setTerm(event.target.value)
+    // console.log(event.target.value)
   };
 
   return (
     <div>
       <form onSubmit={handleSubmitForm}>
-        <input type="text" />
+        <input onChange={handleChange} value={term} type="text" />
       </form>
 
       {/* <button onClick={ () => whenPressEnter ('cars') }> Search </button> */}
